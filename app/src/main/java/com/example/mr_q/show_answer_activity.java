632428e241a4e1a89f2d928ccb3d6821323a7bc8
isimotosyoma <shibenjiangzhen@gmail.com>
2025-10-31 17:46:14 +0900

    回答結果画面の背景と答えの欄と解答欄の足しました

--- app/src/main/java/com/example/mr_q/show_answer_activity.java
+++ app/src/main/java/com/example/mr_q/show_answer_activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class show_answer_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.show_answer_activity);
     }
 }
