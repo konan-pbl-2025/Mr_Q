97dce94027de073fdffeb50fd17dca23f5fcb467
KazuhaTonan <e.s.10000leaf@gmail.com>
2025-10-31 15:48:32 +0900

    StartActivity作成

--- app/src/main/java/com/example/mr_q/StartActivity.java
+++ app/src/main/java/com/example/mr_q/StartActivity.java
@@ -1,8 +1,31 @@
 package com.example.mr_q;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class StartActivity {
+public class StartActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
 
+        Button startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ボタンがクリックされたらMainActivityに遷移する
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();  // StartActivityを終了して戻らないようにする
+            }
+        });
+    }
 }
+
+
+
