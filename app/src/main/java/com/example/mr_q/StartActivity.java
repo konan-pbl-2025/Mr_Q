9cd194a34cb5f943745fa5cd5d3cbbbec0e6bd98
kzk1207 <natto3807@gmail.com>
2025-11-07 14:00:29 +0900

    startActivityで起動するようにした。

--- app/src/main/java/com/example/mr_q/StartActivity.java
+++ app/src/main/java/com/example/mr_q/StartActivity.java
@@ -13,9 +13,9 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.id.questionText);
+        setContentView(R.layout.activity_start);
 
-        Button startButton = findViewById(R.id.activity_start);
+        Button startButton = findViewById(R.id.startButton);
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
