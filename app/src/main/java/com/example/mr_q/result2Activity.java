a0ded2d165d7d8c28f5435209dcf52a05d6611b0
meyonesu <nakaigameproject@gmail.com>
2025-11-28 16:12:30 +0900

    All tasks be done

--- app/src/main/java/com/example/mr_q/result2Activity.java
+++ app/src/main/java/com/example/mr_q/result2Activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class result2Activity extends AppCompatActivity {
 
@@ -14,9 +15,15 @@ public class result2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result2);
         Button rButton = findViewById(R.id.rButton);
+
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setText(String.valueOf("あなたの最終スコアは"+ StartActivity.score + "点です。"));
         rButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // ボタンがクリックされたらMainActivityに遷移する
+                StartActivity.currentQuestionIndex = 0;
+                StartActivity.score = 0;
                 Intent intent = new Intent(result2Activity.this, StartActivity.class);
                 startActivity(intent);
                 //finish();  // StartActivityを終了して戻らないようにする
